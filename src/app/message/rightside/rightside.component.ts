6129d9211c962acb24252f6eed5c0ef9dfb227ac
-g <verma.aman99280@gmail.com>
2025-07-22 17:03:23 +0530

    scroll fix

--- src/app/message/rightside/rightside.component.ts
+++ src/app/message/rightside/rightside.component.ts
@@ -22,6 +22,7 @@ export class RightsideComponent implements AfterViewChecked, OnInit {
   user = '';
   message = '';
   emojis: string[] = ['😀', '😂', '😍', '😢', '👍', '🔥'];
+  shouldScrollToBottom: boolean = true;
 
   @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   emojiPickerIndex: number | null = null;
@@ -82,7 +83,7 @@ export class RightsideComponent implements AfterViewChecked, OnInit {
     }
 
     this.emojiPickerIndex = null;
-
+this.shouldScrollToBottom = false; // Reaction added, don't scroll
     this.chatService.sendReaction(msgId, this.groupName, emoji)
     this.chatService.saveReaction(reactionform).subscribe({
       next: (data: any) => {
@@ -102,42 +103,75 @@ export class RightsideComponent implements AfterViewChecked, OnInit {
     return 'data:image/jpeg;base64,' + image;
   }
   ngAfterViewChecked() {
-    if (this.preventAutoScroll) {  // Reset the flag
-      return; // Skip scroll this time
+    if (this.shouldScrollToBottom) {
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+      this.shouldScrollToBottom = false;
     }
-
-    this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
   }
+
+
 
   goBack() {
     this.location.back();
   }
   ngOnInit() {
+    this.router.paramMap.subscribe(params => {
+      this.groupName = String(params.get('groupname'));
+      this.loadChatData();
+    });
+
+    this.user = this.ServiceSrv.getUserName();
+
     this.chatService.startConnection(this.user, (sender, messageGroup, message) => {
       if (messageGroup === this.groupName) {
         this.messages.push({ groupName: messageGroup, sender, message, sentAt: Date() });
+        this.shouldScrollToBottom = true;
       }
-    }).then(() => {
-      this.chatService.PersonalChat(this.groupName, this.user).subscribe((msgs: any) => {
-        this.messages = msgs;
-        console.log(msgs);
-
-        const conn = this.chatService.connection;
-        if (conn) {
-          conn.on("ReceiveReaction", (messageId: number, reaction: string) => {
-            const msg = this.messages.find(m => m.id === messageId);
-            console.log(reaction);
-            console.log(msg);
-
-            if (msg) {
-              msg.reaction = reaction;
-            }
-          });
-        }
-
-      });
     });
   }
+  loadChatData() {
+    this.chatList = this.ServiceSrv.getChatList();
+
+    for (let index = 0; index < this.chatList.length; index++) {
+      if (this.chatList[index].userName === this.groupName) {
+        this.profilePicture = this.chatList[index].profilePicture;
+        this.fullName = this.chatList[index].fullName;
+        break;
+      }
+    }
+
+    if (!this.chatList || this.chatList.length === 0) {
+      this.ServiceSrv.GetFollowing(this.user).subscribe({
+        next: (data: any) => {
+          this.ServiceSrv.setChatList(data);
+          this.chatList = data;
+          for (let index = 0; index < this.chatList.length; index++) {
+            if (this.chatList[index].userName === this.groupName) {
+              this.profilePicture = this.chatList[index].profilePicture;
+              this.fullName = this.chatList[index].fullName;
+              break;
+            }
+          }
+        },
+        error: (error: any) => console.error(error)
+      });
+    }
+
+    // 👇 Load chat messages
+    this.chatService.PersonalChat(this.groupName, this.user).subscribe((msgs: any) => {
+      this.messages = msgs;
+this.shouldScrollToBottom = true; // Scroll on initial load
+      const conn = this.chatService.connection;
+      if (conn) {
+        conn.off("ReceiveReaction"); // Remove previous handler
+        conn.on("ReceiveReaction", (messageId: number, reaction: string) => {
+          const msg = this.messages.find(m => m.id === messageId);
+          if (msg) msg.reaction = reaction;
+        });
+      }
+    });
+  }
+
 
 
   toggleEmojiPicker(index: number): void {
@@ -160,6 +194,7 @@ export class RightsideComponent implements AfterViewChecked, OnInit {
         sentAt: DateTime.toLocaleString()
       });
       this.newMessage = '';
+      this.shouldScrollToBottom = true;
 
     }
 
